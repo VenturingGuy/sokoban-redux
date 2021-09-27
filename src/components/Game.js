3f3676eec7badcb6b14359fe4d0c2731440144e6
Venturing Guy <LVentura335@Gmail.com>
2021-09-27 12:49:28 -0700

    added rendering context into child components

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { Component } from "react";
 
-class Game extends Component { 
+class Game extends Component {
+    mapChildren () {
+        return React.Children.map(this.props.children, function (child) {
+            return React.addons.cloneWithProps(child, {
+                ctx: this.state.ctx 
+            });
+        }.bind(this));
+    } 
     render() { 
         this.state.ctx && this.state.ctx.clearRect(0, 0, 640, 480);
-            
+        
         return (
            <canvas id="game" width="900" height="600">
               { this.state.ctx ? this.mapChildren() : []}
